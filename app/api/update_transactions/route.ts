85a2cfe3304c5a19a78511bcf0a0cdde9f0b7226
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2025-03-08 04:47:29 -0500

    make all endpoints post to work with retell

--- app/api/update_transactions/route.ts
+++ app/api/update_transactions/route.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
 
-export async function PUT() {
+export async function POST() {
     try {
       const { error } = await supabase
         .from('transactions')
@@ -16,20 +16,4 @@ export async function PUT() {
     } catch (error:any) {
       return new NextResponse(`Error updating transactions: ${error.message}`, { status: 500 });
     }
-}
-export async function POST(req:NextRequest) {
-  try {
-    const body = await req.json();
-    const { id, sent_at, suspicious, location, receiver, amount } = body;
-    
-    const { error } = await supabase
-      .from('transactions')
-      .insert([{ id, sent_at, suspicious, location, receiver, amount }]);
-    
-    if (error) throw error;
-    
-    return new NextResponse('Transaction added successfully', { status: 201 });
-  } catch (error:any) {
-    return new NextResponse(`Error adding transaction: ${error.message}`, { status: 500 });
-  }
 }
