47d50126f58cdf02bc5a9c8e492b6eaad382c3a6
Xiaomin Liu <xl4624@nyu.edu>
2025-03-08 11:18:16 -0500

    Set up shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -47,6 +48,9 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <Button>
+            Testing
+          </Button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
